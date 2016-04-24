992e788d5827a907a6ca90b548913356efbfaceb
Alex <alexbutcher2010@gmail.com>
2016-04-24 20:56:22 +0100

    created sub branch

--- app/src/main/java/com/example/alex/subbranchmergedtomain/MainActivity.java
+++ app/src/main/java/com/example/alex/subbranchmergedtomain/MainActivity.java
@@ -53,4 +53,9 @@ public class MainActivity extends AppCompatActivity {
     public void thisWasCreatedInTheMainBranch() {
         //hello
     }
+
+    public void createdInSubBrnach() {
+
+    }
+
 }
